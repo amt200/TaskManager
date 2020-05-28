83610007e5a6b208312022757d7cc1c9e85762d2
18045945 <aungmoethet2000@gmail.com>
2020-05-28 14:36:50 +0800

    DBHelper -> settled

---
+++ app/src/main/java/com/myapplicationdev/android/taskmanager/DBHelper.java
@@ -0,0 +1,82 @@
+package com.myapplicationdev.android.taskmanager;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "tasks.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_TASK = "task";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TASK_NAME = "name";
+    private static final String COLUMN_TASK_DESCRP = "description";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createNoteTableSql = "CREATE TABLE " + TABLE_TASK + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_TASK_NAME + " TEXT NOT NULL,"
+                + COLUMN_TASK_DESCRP + " TEXT NOT NULL)";
+        db.execSQL(createNoteTableSql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
+        onCreate(db);
+    }
+
+    public long insertTask(String name, String description) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TASK_NAME, name);
+        values.put(COLUMN_TASK_DESCRP, description);
+        long result = db.insert(TABLE_TASK, null, values);
+        db.close();
+        return result;
+    }
+
+    public ArrayList<Task> getAllTasks() {
+
+        ArrayList<Task> tasks = new ArrayList<Task>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns= {COLUMN_ID, COLUMN_TASK_NAME, COLUMN_TASK_DESCRP};
+
+        Cursor cursor = db.query(TABLE_TASK, columns, null, null,
+                null, null, null, null);
+
+        // moveToFirst() moves to first row, null if no records
+        if (cursor.moveToFirst()) {
+            // Loop while moveToNext() points to next row
+            //  and returns true; moveToNext() returns false
+            //  when no more next row to move to
+            do {
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                String description = cursor.getString(2);
+
+                Task task = new Task(id, name, description);
+
+                tasks.add(task);
+
+            } while (cursor.moveToNext());
+        }
+        // Close connection
+        cursor.close();
+        db.close();
+
+        return tasks;
+    }
+
+}
